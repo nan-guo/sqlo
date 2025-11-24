2296f2f43ba4f9a6a36aa5309154ef53060860f2
nan.guo <nan.guo@dailymotion.com>
2025-11-24 22:09:31 +0100

    feat(builder): add global dialect configuration
    - Add Q.set_dialect() and Q.get_dialect() class methods
    
    - Set MySQL as default dialect
    
    - Update all query builders to use Q._default_dialect
    
    - Add unit tests for dialect configuration
    
    BREAKING CHANGE: MySQL is now the default dialect (was None)

--- src/sqlo/builder.py
+++ src/sqlo/builder.py
@@ -5,26 +5,39 @@ from .query.delete import DeleteQuery
 from .query.insert import InsertQuery
 from .query.select import SelectQuery
 from .query.update import UpdateQuery
+from .dialects.mysql import MySQLDialect
 
 
 class Q:
     """Query Builder Factory."""
 
+    _default_dialect = MySQLDialect()
+
+    @classmethod
+    def set_dialect(cls, dialect):
+        """Set the default dialect for all queries."""
+        cls._default_dialect = dialect
+
+    @classmethod
+    def get_dialect(cls):
+        """Get the current default dialect."""
+        return cls._default_dialect
+
     @staticmethod
     def select(*columns: Union[str, Raw, Func]) -> SelectQuery:
-        return SelectQuery(*columns)
+        return SelectQuery(*columns, dialect=Q._default_dialect)
 
     @staticmethod
     def insert_into(table: str) -> InsertQuery:
-        return InsertQuery(table)
+        return InsertQuery(table, dialect=Q._default_dialect)
 
     @staticmethod
     def update(table: str) -> UpdateQuery:
-        return UpdateQuery(table)
+        return UpdateQuery(table, dialect=Q._default_dialect)
 
     @staticmethod
     def delete_from(table: str) -> DeleteQuery:
-        return DeleteQuery(table)
+        return DeleteQuery(table, dialect=Q._default_dialect)
 
     @staticmethod
     def raw(sql: str, params: Any = None) -> Raw:
